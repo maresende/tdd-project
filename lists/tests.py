60b282a2e6d53100cc0ce008dd2ebd0706b96718
matheus.bh2@hotmail.com <matheus.bh2@hotmail.com>
2020-09-13 22:30:36 -0300

    Redirecionando após o POST

--- lists/tests.py
+++ lists/tests.py
@@ -1,24 +1,12 @@
-from django.urls import resolve
 from django.test import TestCase
-from django.http import HttpRequest
+from lists.models import Item
 
-from lists.views import home_page
 
 class HomePageTest(TestCase):
 
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
-
-    def test_home_page_returns_correct_html(self):
-        response = self.client.get('/')
-
-        html = response.content.decode('utf8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>To-Do lists</title>', html)
-        self.assertTrue(html.strip().endswith('</html>'))
-
-        self.assertTemplateUsed(response, 'home.html') 
 
     def test_can_save_a_POST_request(self):
         response = self.client.post('/', data={'item_text': 'A new list item'})
@@ -27,12 +15,13 @@ class HomePageTest(TestCase):
         new_item = Item.objects.first()
         self.assertEqual(new_item.text, 'A new list item')
 
-        self.assertIn('A new list item', response.content.decode())
-        self.assertTemplateUsed(response, 'home.html')
+        self.assertEqual(response.status_code, 302)
+        self.assertEqual(response['location'], '/')
 
     def test_only_saves_items_when_necessary(self):
         self.client.get('/')
         self.assertEqual(Item.objects.count(), 0)
+
 
 class ItemModelTest(TestCase):
 
