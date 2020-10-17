3645fa7b6ed316d8839c4b1d58b41936a474411b
Matheus <matheus.bh2@hotmail.com>
2020-10-17 16:40:29 -0300

    Implementando o Novo Projeto (Listas e Items) - PARTE 5

--- lists/tests.py
+++ lists/tests.py
@@ -8,11 +8,6 @@ class HomePageTest(TestCase):
         response = self.client.get('/lists/the-only-list-in-the-world/')
         self.assertTemplateUsed(response, 'list.html')
 
-    def test_only_saves_items_when_necessary(self):
-        self.client.get('/')
-        self.assertEqual(Item.objects.count(), 0)
-
-
 class ItemModelTest(TestCase):
 
     def test_saving_and_retrieving_items(self):
